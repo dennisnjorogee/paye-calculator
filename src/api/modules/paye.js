7910e37f7b088345851b6b31c5a6fb80590247d3
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-06-26 23:01:40 +0300

    feat: implement PAYE calculation logic with tax brackets and personal relief

--- src/api/modules/paye.js
+++ src/api/modules/paye.js
@@ -1,1 +1,24 @@
-export const calculatePAYE = (grossPay) => {};
+import payeRates from "../config/paye-rates.json" with { type: "json" };
+
+export const calculatePAYE = (taxableIncome) => {
+  //constants
+  const PERSONAL_RELIEF = payeRates["personal-tax-relief"];
+  const TAX_BRACKETS = payeRates["tax-brackets"];
+
+  let remainingIncome = taxableIncome;
+  let incomeTax = 0;
+
+  for (const bracket of TAX_BRACKETS) {
+    if (remainingIncome <= 0) break;
+
+    const taxableAmount = Math.min(remainingIncome, bracket.limit);
+    incomeTax += taxableAmount * bracket.rate;
+    remainingIncome -= taxableAmount;
+  }
+
+  console.log("income tax: " + incomeTax);
+
+  const paye = incomeTax - PERSONAL_RELIEF;
+
+  return { incomeTax, personalRelief: PERSONAL_RELIEF, paye };
+};
